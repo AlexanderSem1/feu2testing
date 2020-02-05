7b3d0368fe7b6a06d4af7bef809b3f14599b8032
Alexander Sem <alexander.sem@icloud.com>
2020-02-05 14:40:32 +0100

    changes files 2

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        The new name of the project is no longer shit!
+        The new name of the project is no longer shit 2!
       </h1>
       <h2 class="subtitle">
         My riveting Nuxt.js project
